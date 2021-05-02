d2a626eed0e5a70de7561485f324de8d2e40db90
cwheatley <cwheatley@diamondfortress.com>
2021-05-02 17:57:45 -0500

    Add Onyx cocoa pod.

---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '9.0'
+
+target 'onyx-ios-swift-example' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for onyx-ios-swift-example
+  pod 'OnyxCamera', '~> 7.0.1'
+end
--- onyx-ios-swift-example/ViewController.swift
+++ onyx-ios-swift-example/ViewController.swift
@@ -12,8 +12,43 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        configOnyx()
     }
 
-
+    func configOnyx(){
+        let onyxConfig: OnyxConfiguration = OnyxConfiguration();
+        onyxConfig.viewController = self
+        onyxConfig.licenseKey = "Your license key here"
+        onyxConfig.returnRawFingerprintImage = false
+        onyxConfig.returnProcessedFingerprintImage = true
+        onyxConfig.returnGrayRawFingerprintImage = false
+        onyxConfig.returnGrayRawWsq = false
+        onyxConfig.returnWsq = false
+        onyxConfig.reticleOrientation = ReticleOrientation(rawValue: 0)
+        onyxConfig.showSpinner = true
+        onyxConfig.useLiveness = false
+        onyxConfig.onyxCallback = onyxCallback
+        onyxConfig.successCallback = onyxSuccessCallback
+        onyxConfig.errorCallback = onyxErrorCallback
+        
+        let onyx: Onyx = Onyx()
+        onyx.doSetup(onyxConfig)
+    }
+    
+    func onyxCallback(configuredOnyx: Onyx?) -> Void {
+        NSLog("Onyx Callback");
+        DispatchQueue.main.async {
+            configuredOnyx?.capture(self);
+        }
+    }
+    
+    func onyxSuccessCallback(onyxResult: OnyxResult?) -> Void {
+        NSLog("Onyx Success Callback");
+    }
+    
+    func onyxErrorCallback(onyxError: OnyxError?) -> Void {
+        NSLog("Onyx Error Callback");
+        NSLog(onyxError?.errorMessage ?? "Onyx returned an error");
+    }
 }
 
